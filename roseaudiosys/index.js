5d1231e738ddad15f58a9f2867751fad1592392c
lelsteph <slelong95@gmail.com>
2026-02-21 18:50:23 +0100

    Complete re-design of the wait system ready function so that volumio does not complain.

--- roseaudiosys/index.js
+++ roseaudiosys/index.js
@@ -4,7 +4,6 @@
 var libQ = require('kew');
 //var fs=require('fs-extra');
 const gpiox = require('@iiot2k/gpiox');
-//var config = new (require('v-conf'))();
 var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
 const logging = false; //Enable or disable logging
@@ -15,10 +14,15 @@ module.exports = roseaudiosys;
 function roseaudiosys(context) {
 	const self = this;
 
+	//Volumio variables
 	self.context = context;
 	self.commandRouter = this.context.coreCommand;
 	self.logger = this.context.logger;
 	self.configManager = this.context.configManager;
+
+	//Rose Audio variables
+	self.audioOutputDeviceName = '';
+	self.volumioReadyGPIO = null;
 
 	self.log("Constructor > Volumio constructor called!");
 }
@@ -102,11 +106,11 @@ roseaudiosys.prototype.getAudioOutputDeviceName = function() {
 		const audioConfigContent = fs.readFileSync(audioConfigPath, 'utf8');
 		const audioConfig = JSON.parse(audioConfigContent);
 
-		self.log("getAudioConfig > Audio Output Device: " + audioConfig.outputdevicename.value, "info");
+		self.log("getAudioOutputDeviceName > Audio Output Device: " + audioConfig.outputdevicename.value, "info");
 
 		return audioConfig.outputdevicename.value;
 	} catch (error) {
-        self.log("getAudioConfig > Failed to get audio configuration: " + error.message, "error");
+        self.log("getAudioOutputDeviceName > Failed to get audio configuration: " + error.message, "error");
         return '';
     }
 }
@@ -134,41 +138,101 @@ roseaudiosys.prototype.sleep = function(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+/**
+ * Wait for the system to be ready by checking the Volumio System Status environment variable at regular intervals.
+ *	https://www.sitepoint.com/delay-sleep-pause-wait/
+ *	https://javascript.info/async-await
+ *  https://github.com/volumio/volumio-plugins-sources-bookworm/blob/4cbedc927025e09508fb8017216b8ac24534f5ab/autostart/index.js
+ */
+roseaudiosys.prototype.waitSystemReadyAndInitialize = function() {
+	const self = this;
+	const SYSTEM_READY_CHECK_INTERVAL = 1000; //Interval in milliseconds to check if system is ready
+	const SYSTEM_READY_MAX_RETRIES = 90; //Maximum number of retries to check if system is ready
+	let retryCount = 0;
+
+	new Promise(async (resolve, reject) => {
+		self.log("waitSystemReadyAndInitialize > Waiting for system to be ready...", "info");
+		while (!self.isSystemReady() && (retryCount < SYSTEM_READY_MAX_RETRIES)) {
+			if (retryCount > 0) self.log("waitSystemReady > Checking if system is ready... Retry count: " + retryCount, "info");
+			await self.sleep(SYSTEM_READY_CHECK_INTERVAL); //Wait for SYSTEM_READY_CHECK_INTERVAL ms before checking again
+			retryCount++;
+		}
+
+		//If the system is not ready after maximum retries, log an error but continue with the plugin initialization.
+		if (retryCount >= SYSTEM_READY_MAX_RETRIES) {
+			//self.log("waitSystemReady > System is not ready after maximum retries!", "error");
+			reject(new Error("waitSystemReadyAndInitialize > System is not ready after all retries!"));
+		} else {
+			self.log("waitSystemReadyAndInitialize > System is ready after " + retryCount + " retries (seconds)!", "info");
+			resolve("waitSystemReadyAndInitialize > System is ready after " + retryCount + " retries (seconds)!");
+		}
+
+		//Initialize the player in all cases, even if the system is not ready, to avoid leaving the Rose Audio Player Ready LED Off
+		self.initializePlayer();
+	}).then(() => {
+		//Initialization successful
+	}).catch((error) => {
+		self.log("waitSystemReadyAndInitialize > Initialization error: " + error.message, "error");
+	});
+}
+
+/**
+ * Initialize the player based on the detected audio output device. This method is called after the system is ready.
+ * @returns true if the player was initialized successfully, false otherwise
+ */
+roseaudiosys.prototype.initializePlayer = function() {
+    const self = this;
+	
+	/*#######################################################################*/
+	/* Manage the GPIO pin to signal Volumio readiness to the Rose Audio hardware.
+	/*#######################################################################*/
+	switch (self.audioOutputDeviceName) {
+		case "RoseAudio Femto Player":
+			//Log applying special configuration for Rose Audio Femto Player
+            self.log("initializePlayer > Applying Rose Audio Femto Player special configuration...", "info");
+			
+			//Retrieve the Volumio Ready GPIO pin number from the plugin configuration
+			self.volumioReadyGPIO = parseInt(self.config.get("femtoPlayerVolumioReadyGPIO.pin"), 10);
+
+			//Make sure the GPIO pin number is valid
+			if (isNaN(self.volumioReadyGPIO)) {
+				self.log("initializePlayer > Invalid GPIO pin number for Volumio Ready signal!", "error");
+				defer.reject(new Error("Invalid GPIO pin number for Volumio Ready signal!"));
+				return defer.promise;
+			}
+
+			//Initialize the Volumio Ready GPIO
+			gpiox.init_gpio(self.volumioReadyGPIO, gpiox.GPIO_MODE_OUTPUT, 0);
+
+			// Set the Volumio Ready GPIO to High State
+			self.log("initializePlayer > Volumio Ready GPIO Number: " + self.volumioReadyGPIO + " set to HIGH!", "info");
+			gpiox.set_gpio(self.volumioReadyGPIO, 1);
+
+			//Log the successful initialization of the Rose Audio Femto Player
+			self.log("initializePlayer > Rose Audio Femto Player initialization completed successfully!", "info", true);
+
+		break;
+	
+		default:
+			// //Warn the user that the audio output device is not supported by this plugin
+	        // self.Log(self.commandRouter.getI18nString("ERROR") + self.commandRouter.getI18nString("PLUGIN_CONFIGURATION") + self.commandRouter.getI18nString("HARDWARE_NOT_SUPPORTED"), "info", true);
+			// self.commandRouter.pushToastMessage(self.commandRouter.getI18nString("ERROR"), self.commandRouter.getI18nString("PLUGIN_CONFIGURATION"), self.commandRouter.getI18nString("HARDWARE_NOT_SUPPORTED") + ": " + self.audioOutputDeviceName + "!");
+	        self.log("initializePlayer > Un-supported hardware: " +self.audioOutputDeviceName, "error", true);
+
+			
+        	return false;
+	}
+
+	return true;
+}
+
 // Plugin is starting
-roseaudiosys.prototype.onStart = async function() {
+roseaudiosys.prototype.onStart = function() {
     const self = this;
 	const defer = libQ.defer();
-	const  volumioReadyGPIO = parseInt(self.config.get("volumioReadyGPIO.pin"), 10);
-	const SYSTEM_READY_CHECK_INTERVAL = 1000; //Interval in milliseconds to check if system is ready
-	const SYSTEM_READY_MAX_RETRIES = 120; //Maximum number of retries to check if system is ready
-	let retryCount = 0;
 	
 	//Log Rose Audio System Initialization Plugin startup
 	self.log("onStart > Starting Rose Audio System Initialization Plugin!", "info", true);
-
-	/*#######################################################################*/
-	/* Wait for system to be ready
-	/*#######################################################################*/
-	//https://www.sitepoint.com/delay-sleep-pause-wait/
-	//https://javascript.info/async-await
-	self.log("onStart > System is not ready yet. Waiting...", "info");
-	while (!self.isSystemReady() && (retryCount < SYSTEM_READY_MAX_RETRIES)) {
-		if (retryCount > 0) self.log("onStart > Checking if system is ready... Retry count: " + retryCount, "info");
-		await self.sleep(SYSTEM_READY_CHECK_INTERVAL); //Wait for SYSTEM_READY_CHECK_INTERVAL ms before checking again
-		retryCount++;
-	}
-	
-	//If the system is not ready after maximum retries, log an error but continue with the plugin initialization.
-	if (retryCount >= SYSTEM_READY_MAX_RETRIES) {
-		self.log("onStart > System is not ready after maximum retries!", "error");
-	} else {
-		self.log("onStart > System is ready after " + retryCount + " retries (seconds)!", "info");
-	}
-
-	/*#######################################################################*/
-	/* Retrieve the audio output device name
-	/*#######################################################################*/
-    const audioOutputDeviceName = self.getAudioOutputDeviceName();
 
 	/*#######################################################################*/
 	/* Disable the HDMI interface to remove any possible source of noise
@@ -187,42 +251,16 @@ roseaudiosys.prototype.onStart = async function() {
 	// });
 
 	/*#######################################################################*/
-	/* Manage the GPIO pin to signal Volumio readiness to the Rose Audio hardware.
+	/* Retrieve the audio output device name
 	/*#######################################################################*/
-	switch (audioOutputDeviceName) {
-		case "RoseAudio Femto Player":
-            self.log("onStart > Audio Output Device: " + audioOutputDeviceName + " found! Applying special configuration...", "info");
+    self.audioOutputDeviceName = self.getAudioOutputDeviceName();
 
-			//Make sure the GPIO pin number is valid
-			if (isNaN(volumioReadyGPIO)) {
-				self.log("onStart > Invalid GPIO pin number for Volumio Ready signal!", "error");
-				defer.reject(new Error("Invalid GPIO pin number for Volumio Ready signal!"));
-				return defer.promise;
-			}
-
-			//Initialize the Volumio Ready GPIO
-			gpiox.init_gpio(volumioReadyGPIO, gpiox.GPIO_MODE_OUTPUT, 0);
-
-			// Set the Volumio Ready GPIO to High State
-			self.log("onStart > Volumio Ready GPIO Number: " + volumioReadyGPIO + " set to HIGH!", "info");
-			gpiox.set_gpio(volumioReadyGPIO, 1);
-
-			//Log the successful initialization of the Rose Audio Femto Player
-			self.log("onStart > Rose Audio Femto Player initialization completed successfully!", "info", true);
-
-			break;
-	
-		default:
-			//Warn the user that the audio output device is not supported by this plugin
-			self.commandRouter.pushToastMessage("error", "Rose Audio System Initialization Plugin", "Un-supported audio output device: " + audioOutputDeviceName);
-	        self.log("onStart > Un-supported hardware: " + audioOutputDeviceName, "error");
-        	defer.reject("Un-supported hardware: " + audioOutputDeviceName);
-
-            return defer.promise;
-	}
+	//Wait for the system to be ready and initialize the player
+	self.waitSystemReadyAndInitialize();
 
 	// Once the Plugin has successfull started resolve the promise
 	defer.resolve();
+	self.log("onStart > Rose Audio System Initialization Plugin started successfully!", "info", true);
 
     return defer.promise;
 };
@@ -231,25 +269,24 @@ roseaudiosys.prototype.onStart = async function() {
 roseaudiosys.prototype.onStop = function() {
     const self = this;
     const defer = libQ.defer();
-	const  volumioReadyGPIO = parseInt(self.config.get("volumioReadyGPIO.pin"), 10);
+	//const  volumioReadyGPIO = parseInt(self.config.get("RoseAudioFemtoPlayerVolumioReadyGPIO.pin"), 10);
    
 	//Log Rose Audio System Initialization Plugin shutdown
 	self.log("onStop > Stopping Rose Audio System Plugin!", "info");
 
-	//Make sure the GPIO pin number is valid
-	if (isNaN(volumioReadyGPIO)) {
-		self.log("onStop > Invalid GPIO pin number for Volumio Ready signal!", "error");
-		defer.resolve(); //Do not fail on stop
-		//defer.reject(new Error("Invalid GPIO pin number for Volumio Ready signal"));
-		return defer.promise;
+	/*#######################################################################*/
+	/* Manage the GPIO pin to signal Volumio readiness to the Rose Audio hardware.
+	/*#######################################################################*/
+	//Assuming all Rose Audio devices has a Volumio Readu GPIO pin. The GPIO pin
+	//corresponding to the specific Rose Audio device is retrieved in the onStart method.
+	if (typeof self.volumioReadyGPIO === "number" && !isNaN(self.volumioReadyGPIO)) {
+		// Set the Volumio Ready GPIO to Low State
+		self.log("onStop > Volumio Ready GPIO Number: " + self.volumioReadyGPIO + " set to LOW!", "info");
+		gpiox.set_gpio(self.volumioReadyGPIO, 0);
+
+		// Deinitialize the Volumio Ready GPIO
+		gpiox.deinit_gpio(self.volumioReadyGPIO);
 	}
-
-	// Set the Volumio Ready GPIO to Low State
-	self.log("onStop > Volumio Ready GPIO Number: " + volumioReadyGPIO + " set to LOW!", "info");
-	gpiox.set_gpio(volumioReadyGPIO, 0);
-
-	// Deinitialize the Volumio Ready GPIO
-	gpiox.deinit_gpio(volumioReadyGPIO);
 
     // Once the Plugin has successfull stopped resolve the promise
     defer.resolve();
@@ -259,7 +296,7 @@ roseaudiosys.prototype.onStop = function() {
 
 // Plugin is restarting
 roseaudiosys.prototype.onRestart = function() {
-    var self = this;
+    const self = this;
     // Optional, use if you need it
 };
 
